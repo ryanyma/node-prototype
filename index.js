bb83025f10bd95cda2872f235445c6a55064825d
Ryan Ma <ryanyma@umich.edu>
2018-01-10 15:18:53 -0500

    jshint syntax error

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('./public/index.html')
-})
+  response.send('./public/index.html');
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
